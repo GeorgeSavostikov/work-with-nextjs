f891a9b93ebadb463543761b3ee14023a3075c37
Heorhii Savostikov <georgesavostikov@gmail.com>
2025-05-31 14:09:43 +0300

    Work with client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,15 @@
-import styles from './page.module.css';
-import course from "./course.json";
-import { Lesson } from './lesson';
+import dynamic from 'next/dynamic';
 
-async function getAllCourses(): Promise<typeof course> {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(course), 1000);
-  });
-}
+const ClientComponent = dynamic(
+  () => import('./some-component'),
+  { ssr: true }
+);
 
 export default async function Home() {
-  const courses = await getAllCourses();
-
   return (
-    <main className={styles.main}>
-      <ul>
-        <h2 className="header">React</h2>
-
-        {courses.lessons.map((lesson) => (
-          <li key={lesson.name}>
-            <Lesson title={lesson.title} shortSummary={lesson.shortSummary} />
-          </li>
-        ))}
-      </ul>
+    <main>
+      <h2>React</h2>
+      <ClientComponent />
     </main>
   );
 }
